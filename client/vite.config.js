259beaaa7d8943aa39a0ea4f419a6366e1888143
rakovinafap <rakovinafap@gmail.com>
2026-05-09 18:59:52 +0300

    Fix Vite build configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['highlight.js']
+  },
   build: {
     rollupOptions: {
-      external: []  // Додайте цей рядок
+      external: []
     }
   },
   server: {
