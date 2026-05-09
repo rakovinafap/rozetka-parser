940a84e6c69688f82e64b73ae213a6ca27cf0afd
rakovinafap <rakovinafap@gmail.com>
2026-05-09 19:54:43 +0300

    Fix: add base path to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: 'index.html'
-    }
+    emptyOutDir: true
   },
   server: {
     proxy: {
